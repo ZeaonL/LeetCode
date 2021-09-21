b16c91226297c4daa33f2c9cdcd2d4a5175b9803
ZeaonL <291943003@qq.com>
2021-09-21 18:38:20 +0800

    upload on 2021.9.21

---
+++ 36.isValidSudoku/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+    }    
+    
+    public Solution () {
+        /*
+        
+        */
+    }
+
+    public static void main(String[] args) {
+        // System.out.println("Hello Solution!");
+        Solution mySolution = new Solution();
+        mySolution.addTwoNumbers(a, b).printList();
+        // System.out.println(Arrays.toString(mySolution.twoSumOther(nums, target)));
+    }
+}
